6aff75cc36df13f698eeb18a584ff24c72fa7fed
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-11-13 20:19:47 +0100

    Setup Classes

---
+++ Prestito.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Prestito
+{
+    public DateTime DataInizio { get; set; }
+    public DateTime DataFine { get; set; }
+
+    public Prestito()
+    {
+        //Un nuovo prestito avrà come data inizio prestito la data corrente e come durata prestabilita una settimana
+        DataInizio = DateTime.Now;
+        DataFine = DateTime.Now.AddDays(7);
+    }
+}
